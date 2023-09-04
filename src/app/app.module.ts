9fab8d54071751e41f68409c0ad04833d8f529a6
mco <mathieu.cordebard@al-enterprise.com>
2023-09-04 13:22:13 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  firebase: {
+    projectId: 'myways-6fe42',
+    appId: '1:1079010931305:web:d151a49e437db58cf728f8',
+    storageBucket: 'myways-6fe42.appspot.com',
+    apiKey: 'AIzaSyAL0eFeeBd0UpLr7J5XY1IZBfFWBVLBQjY',
+    authDomain: 'myways-6fe42.firebaseapp.com',
+    messagingSenderId: '1079010931305',
+    measurementId: 'G-3C40LKNWJT',
+  },
+  apiKey: "AIzaSyAL0eFeeBd0UpLr7J5XY1IZBfFWBVLBQjY",
+  authDomain: "myways-6fe42.firebaseapp.com",
+  projectId: "myways-6fe42",
+  storageBucket: "myways-6fe42.appspot.com",
+  messagingSenderId: "1079010931305",
+  appId: "1:1079010931305:web:d151a49e437db58cf728f8",
+  measurementId: "G-3C40LKNWJT"
+};
